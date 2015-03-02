b9d68d48b870ba03333d8059497a3674fe671abd
elena <elena.15@hotmail.it>
2015-03-02 17:41:39 +0000

    added dropdown menu to first index page

--- public/js/app.js
+++ public/js/app.js
@@ -2,9 +2,4 @@ $(document).ready(function(){
 
   $('#start_text').fadeIn(1000);
 
-  // $('#overview_link').click(function(){
-  //   $('html, body').animate({scrollTop:$('#overview').position().top}, 'slow');
-  // })
-
-
 })
