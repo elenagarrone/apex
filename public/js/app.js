12b3c5a5ea0be70740e878cbcc69db3d3010250b
elena <elena.15@hotmail.it>
2015-03-02 11:52:39 +0000

    added feature

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
 
   $('#start_text').fadeIn(1000);
 
+  // $('#overview_link').click(function(){
+  //   $('html, body').animate({scrollTop:$('#overview').position().top}, 'slow');
+  // })
+
+
 })
